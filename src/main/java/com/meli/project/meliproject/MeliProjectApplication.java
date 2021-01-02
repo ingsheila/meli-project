003fec7307c303b05205b9f715a3cc660959cdce
Sheila Corina Soria <ssoria@baufest.com>
2021-01-02 16:54:10 -0300

    first commit

---
+++ src/main/java/com/meli/project/meliproject/MeliProjectApplication.java
@@ -0,0 +1,13 @@
+package com.meli.project.meliproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliProjectApplication.class, args);
+	}
+
+}
